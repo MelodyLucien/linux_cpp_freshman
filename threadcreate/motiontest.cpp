96a73f6dfe6d81ee4a610ab71a716b4efb22f082
lucien <825038797@qq.com>
2017-03-11 16:26:57 +0800

    1.fix multi thread invocation about jni 2.add onLoad and attach and detach function

--- threadcreate/motiontest.cpp
+++ threadcreate/motiontest.cpp
@@ -1,7 +1,64 @@
 #include<iostream>
 #include<jni.h>
+#include <pthread.h>
 #include<com_hisense_motionkit_MotionMonitorController.h>
+#include "Student.h"
+
+#define FLAG 0
+#undef Flag
+#define Flag 1
+#define NUM_THREADS 5
 using namespace std;
+
+int status = 0;
+bool  isAttached =false;
+JNIEnv * GlobalEnv;
+jclass GlobalJclass;
+JavaVM *cached_jvm;
+
+void* say_hello(void* args)
+{
+    cout << "say_hello GlobalEnv = " << GlobalEnv << endl;
+    status = cached_jvm->AttachCurrentThread((void **)&GlobalEnv, NULL);
+    if(status < 0) {
+       cout<<"callback_handler: failed to attach current thread"<<endl;
+       return (void*)0;
+    }else{
+       isAttached = true;
+    }
+
+    // GlobalEnv = envnow;
+    cout << "say_hello after GlobalEnv = " << GlobalEnv << "GlobalJclass = "<< GlobalJclass << endl;
+
+    zhouhao2::Student stu;
+    stu.introduce();
+
+    //detattch native thread
+    if(isAttached){
+       cached_jvm->DetachCurrentThread();
+    }
+}
+
+
+namespace zhouhao2{
+
+int main(){
+
+	pthread_t tids;
+	int ret = pthread_create(&tids, NULL, say_hello, NULL);
+	if (ret != 0)
+	{
+	   cout << "pthread_create error: error_code=" << ret << endl;
+	}
+
+	//key step
+	pthread_join(tids, NULL);
+
+   return 0;
+}
+
+}
+
 
 /*
  * Class:     com_hisense_motionkit_MotionMonitorController
@@ -9,9 +66,21 @@ using namespace std;
  * Signature: ()V
  */
 JNIEXPORT void JNICALL Java_com_hisense_motionkit_MotionMonitorController_startMonitor
-  (JNIEnv *, jclass){
-       cout<<"i start !!"<<endl;
-  }
+  (JNIEnv *env, jclass cls){
+       GlobalEnv=env;
+       GlobalJclass=cls;
+       cout<<"i start !! GlobalJclass = "<< GlobalJclass << "GlobalEnv = "<< GlobalEnv <<endl;
+
+       //attach thread
+        if(NULL == cached_jvm)
+    {
+        cout<<"g_JVM == NULL"<<endl;
+        return ;
+    }
+
+
+       zhouhao2::main();
+}
 
 /*
  * Class:     com_hisense_motionkit_MotionMonitorController
@@ -21,36 +90,15 @@ JNIEXPORT void JNICALL Java_com_hisense_motionkit_MotionMonitorController_startM
 JNIEXPORT void JNICALL Java_com_hisense_motionkit_MotionMonitorController_stopMonitor
   (JNIEnv *, jclass){
       cout<<"i stop !!"<<endl;
+
   }
 
-class Student{
-public :
- Student(int ,int );
- void introduce();
+  JNIEXPORT jint JNICALL
+JNI_OnLoad(JavaVM *jvm, void *reserved)
+{
 
-private:
-  int number;
-  int score;
-};
+cached_jvm = jvm; /* cache the JavaVM pointer */
 
-Student::Student(int m, int s):number(m),score(s){}
-
-void Student::introduce(){
-   cout<<"my number is  "<<number<<" my score is "<<score<<endl;
+return JNI_VERSION_1_8;
 }
-
-int main(){
-   Student stu[5]={Student(1,60),Student(2,70),Student(3,80),Student(4,90),Student(5,100)};
-   Student * p=&stu[0];
-   //Student *p =stu; is also right
-   for(int i=0;i<5;i++){
-     if(((i+1)%2)==1)
-     stu[i].introduce();
-   }
-
-   p->introduce();
-
-   return 0;
-}
-
 
