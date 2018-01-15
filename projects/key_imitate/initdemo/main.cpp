ce95a43fc44f7e054003305e8cf187ab0c8c5239
Lucien <825038797@qq.com>
2018-01-15 14:48:31 +0800

    last sync

--- projects/key_imitate/initdemo/main.cpp
+++ projects/key_imitate/initdemo/main.cpp
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mount.h>
+#include <stdio.h>
 
 using namespace std;
 
@@ -12,6 +13,18 @@ int main()
     mkdir("/home/zhouhao2/myGit/dirinit/mount",0755);
     mount("tmpfszhouhao2","/home/zhouhao2/myGit/dirinit/mount","tmpfszhouhao2",MS_NOSUID,"mode=0755");
 
-    cout << "Hello world!" << endl;
+    const char *p1 = "hello1";
+
+    printf("%s\n",p1);
+
+   // p1[0]='g'; wrong
+
+
+    char * const p2 = "hello2";
+
+     // p2[0]='g';
+
+    printf("%s\n",p2);
+
     return 0;
 }
